53af06af653ef3946ca02e36e0a1e4a1900f968b
ArmentaSergio <146971020+ArmentaSergio@users.noreply.github.com>
2023-10-20 15:21:25 -0700

    lab4 completed

---
+++ lab4/js/lab4.js
@@ -0,0 +1,19 @@
+// index.js - Pseudocoding and Problem solving          
+// Author: Sergio Armenta
+// Date: 10-20-2023
+
+//Dealer shuffles cards
+//Each player gets 2 cards
+//PLAYERS TURN
+    // Add sum of cards
+    //If sums of cards is less than 21, continue
+        //Player can hit or stay
+        //If player stays go to dealers turn
+    //If sum is greater than or equal to game is over
+    //If sum is 21 or less, continue to dealers turn
+//DEALERS TURN
+    //Add sum of cards
+    //Check if 17 or greater
+        //If yes, dealer must stay
+        //If no, dealer must hit
+    //If no one busts, whoever has higher total wins

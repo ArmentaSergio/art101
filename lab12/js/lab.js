93fa8386b07a0b74875d9837e3ff64083d9b103f
ArmentaSergio <146971020+ArmentaSergio@users.noreply.github.com>
2023-11-22 12:53:43 -0800

    lab12_completed

---
+++ lab12/js/lab.js
@@ -0,0 +1,35 @@
+// index.js - Lab  12 Conditionals
+// Author: Sergio Armenta
+// Date: 11/22/2023
+
+//sort function 
+//takes a string as an argument and returns a string
+function sortingHat(str) {
+    console.log("You are being sorted in a house.");
+    
+    //getting length of argument string
+    var numLetters = str.length;
+    
+    var mod = numLetters % 4;
+
+    //conditional expression 
+    if(mod == 0) {
+        return "Gryffindor";
+    }
+    else if (mod == 1) {
+        return "Ravenclaw";
+    }
+    else if (mod == 2) {
+        return "Slytherin";
+    }
+    else if (mod == 3) {
+        return "Hufflepuff";
+    }
+}
+
+$("#button").click(function () {
+    //getting value from input
+    const name = $("#input").val();
+    house = sortingHat(name);
+    $("#output").html("<div id = 'text'><p> The Sorting Hat has sorted you into " + house + "!</p></div>");
+});
